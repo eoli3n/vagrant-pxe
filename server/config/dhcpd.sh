e83e298298036648b698c6dde58876068f24931e
eoli3n <jkirsz@gmail.com>
2020-11-21 02:34:04 +0100

    ipxe client boot works

--- server/config/dhcpd.sh
+++ server/config/dhcpd.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 ### DHCPD configuration
+ifup eth1
+# Copy first to configure isc-dhcp-server on eth1
 apt install isc-dhcp-server -y
+cp /config/ressources/dhcpd/isc-dhcp-server /etc/default/isc-dhcp-server
 cp /config/ressources/dhcpd/dhcpd.conf /etc/dhcp/dhcpd.conf
 cp /config/ressources/dhcpd/ipxe.conf /etc/dhcp/ipxe.conf
-cp /config/ressources/dhcpd/isc-dhcp-server /etc/default/isc-dhcp-server
 systemctl restart isc-dhcp-server
--- server/config/ipxe.sh
+++ server/config/ipxe.sh
@@ -2,3 +2,4 @@
 mkdir /tftpboot/ipxe
 wget http://boot.ipxe.org/ipxe.efi -O /tftpboot/ipxe/ipxe.efi
 wget http://boot.ipxe.org/undionly.kpxe -o /tftpboot/ipxe/undionly.kpxe
+cp /config/ressources/ipxe/init.ipxe /tftpboot/ipxe/init.ipxe
